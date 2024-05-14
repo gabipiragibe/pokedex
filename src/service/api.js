afa3bbb6208ee7e2102aad670b6a8d80c44c8c27
gabriela.piragibe <gabriela.piragibe@ifood.com.br>
2024-05-14 10:28:35 -0300

    first commit

---
+++ src/service/api.js
@@ -0,0 +1,9 @@
+// import axios from 'axios';
+
+// axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
+//   .then(function (response) {
+
+//     console.log(response);
+//   })
+// axios.get(`pokemon.com/${valordoinput}`)
+
