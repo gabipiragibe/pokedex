a519a0414da7297032ec53f35a4fa8650753f0ac
gabriela.piragibe <piragibegabriela@gmail.com>
2024-08-29 22:55:54 -0300

    Initial commit for test file of ButtonNavigation component

--- src/components/ButtonNavigation/tests/index.spec.jsx
+++ src/components/ButtonNavigation/tests/index.spec.jsx
@@ -1,6 +1,20 @@
-describe("Test Button", () => {
-  it("test render button", () => {
-    expect(1).toBe(1);
-  });
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { I18nextProvider } from "react-i18next";
+import i18n from "i18next";
+import { BrowserRouter as Router } from "react-router-dom";
+import ButtonNavigation from "../index";
+import "@testing-library/jest-dom/extend-expect";
+
+it("should render ButtonNavigation", () => {
+  render(
+    <I18nextProvider i18n={i18n}>
+      <Router>
+        <ButtonNavigation />
+      </Router>
+    </I18nextProvider>
+  );
+
+  expect(screen.getByText("Anterior ID:")).toBeInTheDocument();
+  expect(screen.getByText("Próximo ID:")).toBeInTheDocument();
 });
-//arquivo de teste de uso do jest
