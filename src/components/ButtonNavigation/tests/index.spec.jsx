650d87ef5a702afa5d3a26b65aadb3f05a91bbbb
gabriela.piragibe <piragibegabriela@gmail.com>
2024-08-31 20:48:31 -0300

    feat: tests in progress

--- src/components/ButtonNavigation/tests/index.spec.jsx
+++ src/components/ButtonNavigation/tests/index.spec.jsx
@@ -5,16 +5,38 @@ import i18n from "i18next";
 import { BrowserRouter as Router } from "react-router-dom";
 import ButtonNavigation from "../index";
 import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
 
-it("should render ButtonNavigation", () => {
-  render(
-    <I18nextProvider i18n={i18n}>
-      <Router>
-        <ButtonNavigation />
-      </Router>
-    </I18nextProvider>
-  );
+i18n.init({
+  lng: "pt",
+  resources: {
+    pt: {
+      translation: {
+        buttons: {
+          previous_id: "Anterior ID",
+          next_id: "Próximo ID",
+        },
+      },
+    },
+  },
+});
 
-  expect(screen.getByText("Anterior ID:")).toBeInTheDocument();
-  expect(screen.getByText("Próximo ID:")).toBeInTheDocument();
+it("<ButtonNavigation />", () => {
+  const renderWithProviders = (component) =>
+    render(
+      <I18nextProvider i18n={i18n}>
+        <Router>{component}</Router>
+      </I18nextProvider>
+    );
+
+  renderWithProviders(<ButtonNavigation />);
+
+  const buttonNext = screen.getByRole("button", { name: "Próximo ID" });
+  const buttonPrevious = screen.getByRole("button", { name: "Anterior ID" });
+
+  userEvent.click(buttonNext);
+  userEvent.click(buttonPrevious);
+
+  expect(screen.getByText("Anterior ID")).toBeInTheDocument();
+  expect(screen.getByText("Próximo ID")).toBeInTheDocument();
 });
