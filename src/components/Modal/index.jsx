f34911e879dd8094bf53eff8e87960a08e12dfef
gabriela.piragibe <piragibegabriela@gmail.com>
2024-09-21 23:44:08 -0300

    feat: [#12]Add habitat input filter

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,47 +1,68 @@
 import React, { useState, useEffect } from "react";
 import * as S from "./styles";
-import { getPokemonType } from "../../service/getPokemonDetails";
+import { getPokemon } from "../../service/getPokemonDetails";
 
 export const Modal = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [type, setType] = useState("");
-  const [pokemonsData, setPokemonsData] = useState([]);
+  const [type, setType] = useState(""); //Armazena o tipo de Pokémon.
+  const [habitat, setHabitat] = useState("");
+  const [pokemonsData, setPokemonsData] = useState([]); //Armazena lista de Pokémons por tipo.
+  const [habitatData, setHabitatData] = useState([]);
 
   const openModal = () => setModalIsOpen(true);
   const closeModal = () => setModalIsOpen(false);
 
-  const fetchPokemonsByType = async (type) => {
-    if (type) {
+  const fetchPokemonsByType = async (type, habitat) => {
+    if (type || habitat) {
       try {
-        const response = await getPokemonType(type.toLowerCase());
+        const response = await getPokemon(
+          type.toLowerCase(),
+          habitat.toLowerCase()
+        );
         if (response) {
-          setPokemonsData(response.pokemon || []);
+          setPokemonsData(response.typeData.pokemon || []);
+          setHabitatData(response.habitatData.pokemon_species || []);
         } else {
           setPokemonsData([]);
-          console.error("Tipo não encontrado, busque outro.");
+          setHabitatData([]);
         }
       } catch (error) {
         setPokemonsData([]);
+        setHabitatData([]);
         console.error(error);
       }
     } else {
       setPokemonsData([]);
+      setHabitatData([]);
     }
   };
 
   useEffect(() => {
-    fetchPokemonsByType(type);
-  }, []);
+    fetchPokemonsByType(type, habitat);
+  }, [type, habitat]);
 
   const onTypeChange = (event) => setType(event.target.value);
+  const onHabitatChange = (event) => setHabitat(event.target.value);
 
   const filterFieldsInput = [
-    { label: "Tipo", name: "type", value: type, onChange: onTypeChange },
+    {
+      label: "Tipo",
+      name: "type",
+      value: type,
+      onChange: onTypeChange,
+    },
+    {
+      label: "Habitat",
+      name: "habitat",
+      value: habitat,
+      onChange: onHabitatChange,
+    },
   ];
 
   const filteredFieldsInput = filterFieldsInput.map(
     ({ label, name, value, onChange }) => (
       <label key={name}>
+        <br />
         {label}:
         <input type="text" name={name} value={value} onChange={onChange} />
       </label>
@@ -49,6 +70,7 @@ export const Modal = () => {
   );
 
   const limitedPokemonsData = pokemonsData.slice(0, 5);
+  const limitedHabitatData = habitatData.slice(0, 5);
 
   return (
     <S.Container>
@@ -57,20 +79,24 @@ export const Modal = () => {
         <h2>Filtrar Pokémons</h2>
         <form onSubmit={(e) => e.preventDefault()}>
           {filteredFieldsInput}
-          <button type="button" onClick={() => fetchPokemonsByType(type)}>
+          <button
+            type="button"
+            onClick={() => fetchPokemonsByType(type, habitat)}
+          >
             Filtrar
           </button>
         </form>
         <div>
-          {limitedPokemonsData.length ? (
-            <ul>
-              {limitedPokemonsData.map((pokemon, index) => (
-                <li key={index}>{pokemon.pokemon.name}</li>
-              ))}
-            </ul>
-          ) : (
-            <p>Nenhum Pokémon encontrado</p>
-          )}
+          <ul>
+            {limitedPokemonsData.map((pokemon, index) => (
+              <li key={index}>{pokemon.pokemon.name}</li>
+            ))}
+          </ul>
+          <ul>
+            {limitedHabitatData.map((species, index) => (
+              <li key={index}>{species.name}</li>
+            ))}
+          </ul>
         </div>
       </S.modalContainer>
     </S.Container>
--- src/service/getPokemonDetails.js
+++ src/service/getPokemonDetails.js
@@ -13,14 +13,20 @@ export const getPokemonDetails = (uuid) => {
   return data;
 };
 
-export const getPokemonType = (type) => {
-  const data = axios
-    .get(`https://pokeapi.co/api/v2/type/${type}`)
-    .then(function (response) {
-      return response.data;
-    })
-    .catch(function (error) {
-      return null;
-    });
-  return data;
+export const getPokemon = async (type, habitat) => {
+  try {
+    const typeResponse = await axios.get(
+      `https://pokeapi.co/api/v2/type/${type}`
+    );
+    const habitatResponse = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon-habitat/${habitat}`
+    );
+
+    return {
+      typeData: typeResponse.data,
+      habitatData: habitatResponse.data,
+    };
+  } catch (error) {
+    return null;
+  }
 };
